26ed5ad0ae457fc5973e5f005879e28070b1057b
Simone Gentili <simone.gentili@antreem.com>
2021-07-26 02:29:23 +0200

    Remove fugitive.

--- install.sh
+++ install.sh
@@ -2,11 +2,3 @@
 rm -rf ~/.vim/pack/vendor/start/nerdtree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
-
-# fugitive
-rm -rf ~/.vim/pack/tpope/start
-mkdir -p ~/.vim/pack/tpope/start
-cd ~/.vim/pack/tpope/start
-git clone https://tpope.io/vim/fugitive.git
-vim -u NONE -c "helptags fugitive/doc" -c q
-
