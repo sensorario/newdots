fd8098e733bc0eb22a736a2e455f1ae302ed70ff
Simone Gentili <simone.gentili@antreem.com>
2021-07-27 18:08:07 +0200

    Single file installer!

--- install.sh
+++ install.sh
@@ -10,5 +10,47 @@ git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/vendor/star
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
 
 
+# nerdtree plugin
+rm -rf ~/.vim/pack/vendor/start/nerdtree-git-plugin
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/pack/vendor/start/nerdtree-git-plugin
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree-git-plugin/doc" -c q
 
-cp vimrc ~/.vimrc
+
+
+
+cat > ~/.vimrc<< EOF
+
+" Tabs and spaces
+set expandtab
+set shiftwidth=4
+set smarttab
+set softtabstop=4
+set tabstop=4
+
+" Map the ledaer
+let mapleader = "\\\"
+
+" NerdTree configuration
+nnoremap <leader>n :NERDTreeFocus<CR>
+nnoremap <C-n> :NERDTree<CR>
+nnoremap <C-t> :NERDTreeToggle<CR>
+nnoremap <C-f> :NERDTreeFind<CR>
+
+let g:NERDTreeDirArrowExpandable = '▸'
+let g:NERDTreeDirArrowCollapsible = '▾'
+let g:NERDTreeGitStatusIndicatorMapCustom = {
+                \ 'Modified'  :'✹',
+                \ 'Staged'    :'✚',
+                \ 'Untracked' :'✭',
+                \ 'Renamed'   :'➜',
+                \ 'Unmerged'  :'═',
+                \ 'Deleted'   :'✖',
+                \ 'Dirty'     :'✗',
+                \ 'Ignored'   :'☒',
+                \ 'Clean'     :'✔︎',
+                \ 'Unknown'   :'?',
+                \ }
+
+let g:NERDTreeGitStatusUseNerdFonts = 1 " you should install nerdfonts by yourself. default: 0
+let g:NERDTreeGitStatusShowIgnored = 1 " a heavy feature may cost much more time. default: 0
+
