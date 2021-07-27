79bae8d3b176d4de45a7f394a288d82bc0596970
Simone Gentili <simone.gentili@antreem.com>
2021-07-27 16:51:44 +0200

    Add vim airline plugin.

--- install.sh
+++ install.sh
@@ -2,3 +2,13 @@
 rm -rf ~/.vim/pack/vendor/start/nerdtree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+
+# vim-airline
+rm -rf ~/.vim/pack/vendor/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/vendor/start/vim-airline
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
+
+
+
+cp vimrc ~/.vimrc
--- vimrc
+++ vimrc
@@ -4,3 +4,16 @@ set shiftwidth=4
 set smarttab
 set softtabstop=4
 set tabstop=4
+
+" Map the ledaer
+let mapleader = "\\"
+
+" NerdTree configuration
+nnoremap <leader>n :NERDTreeFocus<CR>
+nnoremap <C-n> :NERDTree<CR>
+nnoremap <C-t> :NERDTreeToggle<CR>
+nnoremap <C-f> :NERDTreeFind<CR>
+
+let g:NERDTreeDirArrowExpandable = '▸'
+let g:NERDTreeDirArrowCollapsible = '▾'
+
